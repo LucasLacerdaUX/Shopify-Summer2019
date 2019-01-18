c55f644ad82d2360627def95fa3677342190f6e1
Lucas Lacerda <lucas.lacerda@eng.ci.ufpb.br>
2019-01-18 17:34:43 -0300

    Updates

---
+++ precache-manifest.622c03f5dbc0a0996553e946b1e6be52.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "584fd6515a580ff9fc86de62c196978c",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/media/Star.584fd651.svg"
+  },
+  {
+    "revision": "9eda3448985af6ab827fd243842f004b",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/media/Search.9eda3448.svg"
+  },
+  {
+    "revision": "d770d5b98975b88a03fc0e84619ef235",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/media/LoadingIcon.d770d5b9.svg"
+  },
+  {
+    "revision": "0858b67582bf794930c3dfb8e396a98c",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/media/ErrorIcon.0858b675.svg"
+  },
+  {
+    "revision": "eb631f4c5050bc978481629ef3bcf32e",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/media/EmptyIcon.eb631f4c.svg"
+  },
+  {
+    "revision": "789a841b150c974280c7",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/js/runtime~main.789a841b.js"
+  },
+  {
+    "revision": "d0caa1ad04d4b0a9b27a",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/js/main.d0caa1ad.chunk.js"
+  },
+  {
+    "revision": "28e5185554493ecb8c3a",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/js/1.28e51855.chunk.js"
+  },
+  {
+    "revision": "d0caa1ad04d4b0a9b27a",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/static/css/main.f4b823f7.chunk.css"
+  },
+  {
+    "revision": "c058eddbe1f1658a4a3179640c1361fc",
+    "url": "/LucasLacerdaUX/Shopify-Summer2019/index.html"
+  }
+];
